ab056a5e30854b7df3b2a09187d4882842fe6e11
GiorgosTs19 <geontsou52@gmail.com>
2025-05-04 18:09:15 +0300

    Fixed an issue causing the wrong hero bg-image to be loaded on larger screens.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,7 @@
 export function Hero() {
   return (
     <section
-      className={
-        `relative hero-mobile sm:hero-tablet lg:hero-desktop ` +
-        'bg-cover bg-no-repeat bg-center h-dvh w-full flex items-center justify-center rounded-b-3xl'
-      }
+      className={`relative hero-bg bg-cover bg-no-repeat bg-center h-dvh w-full flex items-center justify-center rounded-b-3xl`}
     >
       <div className="absolute bottom-0 left-0 w-full h-48 bg-gradient-to-b from-transparent to-[#E7E4DF] z-10 pointer-events-none rounded-b-3xl" />
       <div className="text-white text-center px-4">
